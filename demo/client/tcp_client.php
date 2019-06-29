0877f6c91b5d5d5470858b7ef95e65b0ae95186a
huangyinghao <wuming_@aliyun.com>
2019-06-29 13:53:54 +0800

    1

---
+++ demo/client/tcp_client.php
@@ -0,0 +1,24 @@
+<?php
+
+//连接 swoole tcp服务
+
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+
+if(!$client->connect("127.0.0.1",9501)){
+    echo '连接失败';
+    exit;
+}
+
+//php cli常量
+fwrite(STDOUT,"请输入消息");
+
+$msg = trim(fgets(STDIN));
+
+//发送消息给 tcp server服务器
+
+$client->send($msg);
+
+
+$result = $client->recv();
+
+echo $result;
